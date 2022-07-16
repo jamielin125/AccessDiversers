3331d32e3881a8027383c84b801c018d91aa6a15
jamielin125 <jamielin125@alum.ccu.edu.tw>
2022-07-16 19:04:35 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+	<h1 class="text-3xl font-bold underline">
+		Hello world!
+	</h1>
+  <HelloWorld />
+</template>
+
+<style scoped>
+</style>
